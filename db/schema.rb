fecf59b58d5f1f66a916e0ddcfd9615bd3d7d259
jendiamond <thejendiamond@gmail.com>
2017-04-04 18:58:19 -0700

    Fix Recipe schema & Add Validations for Recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403050839) do
+ActiveRecord::Schema.define(version: 20170404235407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20170403050839) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
