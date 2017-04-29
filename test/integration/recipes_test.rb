000cb62e1f6c383036b1e52c255ad0c1d49599fd
jendiamond <thejendiamond@gmail.com>
2017-04-28 21:50:34 -0700

    Add show page and styling

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -17,11 +17,19 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should get a list of recipe' do
+  test 'should get a list of recipes' do
     get recipes_path
     assert_template 'recipes/index'
-    assert_match @recipe1.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_select 'a[href=?]', recipe_path(@recipe1), text: @recipe1.name
+    assert_select 'a[href=?]', recipe_path(@recipe2), text: @recipe2.name
   end
 
+  test 'should get recipes show page' do
+    get recipe_path(@recipe1)
+    assert_response :success
+    assert_template 'recipes/show'
+    assert_match @recipe1.name.capitalize, response.body
+    assert_match @recipe1.description, response.body
+    assert_match @chef.chefname, response.body
+  end
 end
